67f49cad0fa8bf188f89528925dd44ee1cf64c90
maithri911 <shettymaithri745@gmail.com>
2024-12-20 19:11:10 +0530

    Add files via upload

---
+++ PizzaBillGenerator/src/Main.java
@@ -0,0 +1,17 @@
+
+public class Main{
+    public static void main(String[] args){
+       // Pizza basePizza = new Pizza(true);
+       // basePizza.addExtraToppings();
+       // basePizza.addExtraCheese();
+       // basePizza.takeAway();
+       // basePizza.getBill();
+
+        DeluxPizza dp = new DeluxPizza(false);
+        dp.takeAway();
+        //dp.addExtraCheese();
+        //dp.addExtraToppings();
+        dp.getBill();
+
+    }
+}
